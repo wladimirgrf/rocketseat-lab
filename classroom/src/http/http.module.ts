122a5c43a3dae59f68dd393be2552659bbaccfa3
wladimirgrf <wladimir.code@gmail.com>
2022-06-05 20:45:59 -0300

    feat: classroom, mirror setup

---
+++ classroom/src/http/http.module.ts
@@ -0,0 +1,19 @@
+import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+import { GraphQLModule } from '@nestjs/graphql';
+import { DatabaseModule } from '../database/database.module';
+import { TestResolver } from './test.resolver';
+import { ApolloDriver } from '@nestjs/apollo';
+
+@Module({
+  imports: [
+    ConfigModule.forRoot(),
+    DatabaseModule,
+    GraphQLModule.forRoot({
+      driver: ApolloDriver,
+      autoSchemaFile: 'src/schema.gql',
+    }),
+  ],
+  providers: [TestResolver],
+})
+export class HttpModule {}
