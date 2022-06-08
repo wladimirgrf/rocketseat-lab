65a6729fee67aae5f0a736a5c8ba6591dc83f342
wladimirgrf <wladimir.code@gmail.com>
2022-06-08 20:54:48 -0300

    feat: purchases, create customers service

---
+++ purchases/src/services/customers.service.ts
@@ -0,0 +1,24 @@
+import { Injectable } from '@nestjs/common';
+
+import { PrismaService } from '../database/prisma/prisma.service';
+
+interface CreateCustomerParams {
+  authUserId: string;
+}
+
+@Injectable()
+export class CustomersService {
+  constructor(private readonly prisma: PrismaService) {}
+
+  async findCustomerByAuthUserId(authUserId: string) {
+    return this.prisma.customer.findUnique({ where: { authUserId } });
+  }
+
+  async createCustomer({ authUserId }: CreateCustomerParams) {
+    return this.prisma.customer.create({
+      data: {
+        authUserId,
+      },
+    });
+  }
+}
