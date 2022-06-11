e47130e7a20c2be9a70f154867569d621f68c069
wladimirgrf <wladimir.code@gmail.com>
2022-06-11 12:45:47 -0300

    feat: classroom, courses, enrollments and students resolvers and services

--- classroom/src/http/http.module.ts
+++ classroom/src/http/http.module.ts
@@ -2,8 +2,13 @@ import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 import { GraphQLModule } from '@nestjs/graphql';
 import { DatabaseModule } from '../database/database.module';
-import { TestResolver } from './test.resolver';
 import { ApolloDriver } from '@nestjs/apollo';
+import { CoursesResolver } from './graphql/resolvers/courses.resolver';
+import { StudentsResolver } from './graphql/resolvers/students.resolver';
+import { EnrollmentsResolver } from './graphql/resolvers/enrollments.resolver';
+import { CoursesService } from '../services/courses.service';
+import { StudentsService } from '../services/students.service';
+import { EnrollmentsService } from '../services/enrollments.service';
 
 @Module({
   imports: [
@@ -14,6 +19,16 @@ import { ApolloDriver } from '@nestjs/apollo';
       autoSchemaFile: 'src/schema.gql',
     }),
   ],
-  providers: [TestResolver],
+  providers: [
+    // GraphQL resolvers
+    CoursesResolver,
+    StudentsResolver,
+    EnrollmentsResolver,
+
+    // GraphQL services
+    CoursesService,
+    StudentsService,
+    EnrollmentsService,
+  ],
 })
 export class HttpModule {}
