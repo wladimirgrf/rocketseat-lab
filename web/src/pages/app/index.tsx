f4d1cd51523e1dcb7a27eea07617cfbfc96a2e88
wladimirgrf <wladimir.code@gmail.com>
2022-06-14 20:43:47 -0300

    feat: web, apllo client config

--- web/src/pages/app/index.tsx
+++ web/src/pages/app/index.tsx
@@ -1,10 +1,25 @@
+import { gql, useQuery } from "@apollo/client"
 import { useUser, withPageAuthRequired } from "@auth0/nextjs-auth0"
+import { withApollo } from "../../lib/withApollo";
 
-export default function Home() {
+const PRODUCTS_QUERY = gql`
+  query GetProducts {
+    products {
+      id,
+      title
+    }
+  }
+`
+
+function Home() {
   const { user } = useUser();
+  const { data, loading, error } = useQuery(PRODUCTS_QUERY);
 
   return (
     <div>
+      <pre>
+        {JSON.stringify(data, null, 2)}
+      </pre>
       <pre>
         { JSON.stringify(user, null, 2) }
       </pre>
@@ -13,3 +28,5 @@ export default function Home() {
 }
 
 export const getServerSideProps = withPageAuthRequired();
+
+export default withApollo(Home);
