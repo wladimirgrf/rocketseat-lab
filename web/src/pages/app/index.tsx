a61d1278ce5d3e4c771c748d5a91ca15a01e9723
wladimirgrf <wladimir.code@gmail.com>
2022-06-15 20:39:25 -0300

    feat: web, auth request and tailwindcss

--- web/src/pages/app/index.tsx
+++ web/src/pages/app/index.tsx
@@ -1,16 +1,20 @@
 import { useUser, withPageAuthRequired } from "@auth0/nextjs-auth0"
-import { useGetProductsQuery } from "../../graphql/generated/graphql";
+import { useMeQuery } from "../../graphql/generated/graphql";
 import { getServerPageGetProducts, ssrGetProducts } from "../../graphql/generated/page";
 import { withApollo } from "../../lib/withApollo";
 
 function Home({ data }) {
   const { user } = useUser();
+  const { data: me } = useMeQuery();
 
   return (
     <div>
       <pre>
-        {JSON.stringify(data, null, 2)}
+        {JSON.stringify(me, null, 2)}
       </pre>
+      {/* <pre>
+        {JSON.stringify(data, null, 2)}
+      </pre> */}
       <pre>
         { JSON.stringify(user, null, 2) }
       </pre>
@@ -19,9 +23,12 @@ function Home({ data }) {
 }
 
 export const getServerSideProps = withPageAuthRequired({
-  getServerSideProps: async (ctx) => {
-    return getServerPageGetProducts({}, ctx);
-  }
+   getServerSideProps: async (ctx) => {
+     //return getServerPageGetProducts({}, ctx);
+     return {
+      props: {}
+     }
+   }
 });
 
 export default withApollo(
